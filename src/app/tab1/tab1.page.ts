a362c0f1a6ff3493f2054eecc758349e133813eb
patrick pu <pxlxinzhao126@gmail.com>
2020-03-18 22:48:25 -0400

    add flip number

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Flip } from 'number-flip';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  $: any;
 
-  constructor() {}
+  constructor() {
+    this.$ = (_) => document.querySelector(_);
+  }
 
+  ionViewWillEnter() {
+    this.showConfirmed();
+  }
+
+  showConfirmed() {
+    const count = 4789;
+    const flip = new Flip({
+      node: this.$('.confirmed'),
+      from: this.topUpNumber(count),
+      duration: 2
+    });
+    flip.flipTo({
+      to: count,
+      direct: false
+    });
+  }
+
+  topUpNumber(i: number) {
+    let size = i.toString().length;
+    let topNumber = '';
+
+    while ( size > 0 ) {
+      topNumber += '9';
+      size--;
+    }
+
+    return +topNumber;
+  }
 }
